2638902548673bdea80e5457580ac88554dfbe09
serper35 <serper.wolf@gmail.com>
2023-09-06 20:31:58 +0300

    solved

---
+++ src/Author.java
@@ -0,0 +1,24 @@
+public class Author {
+    private final String firstName;
+    private final String secondName;
+
+    public Author(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String toString() {
+        return firstName + " " + secondName;
+    }
+
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author authorHP = new Author("Joanne", "Rowling");
+        System.out.println("authorHP.getSecondName() = " + authorHP.getSecondName());
+        System.out.println("authorHP.toString() = " + authorHP.toString());
+        Book harryPotter = new Book(authorHP, "Harry Potter", 1999);
+        System.out.println("harryPotter.getAuthor() = " + harryPotter.getAuthor());
+
+        Author authorTG = new Author("Dmitriy",  "Emec");
+        Book tanyaGrotter = new Book(authorTG, "TanyaGrotter", 2002);
+        System.out.println("Tanya Grotter year of publication - " + tanyaGrotter.getYearOfPublication());
+        tanyaGrotter.setYearOfPublication(2009);
+        System.out.println("Tanya Grotter year of publication - " + tanyaGrotter.getYearOfPublication());
+        System.out.println("tanyaGrotter = " + tanyaGrotter);
     }
 }
